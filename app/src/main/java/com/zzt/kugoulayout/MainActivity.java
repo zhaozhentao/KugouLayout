056fcd492b23b59ddab1544de2633c36e7459581
招振涛 <514656282@qq.com>
2015-02-14 23:19:47 +0800

    小修改

--- app/src/main/java/com/zzt/kugoulayout/MainActivity.java
+++ app/src/main/java/com/zzt/kugoulayout/MainActivity.java
@@ -10,16 +10,16 @@ import com.zzt.library.KugouLayout;
 
 public class MainActivity extends ActionBarActivity {
 
-    KugouLayout circleSwipeLayout;
+    KugouLayout kugouLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        circleSwipeLayout = KugouLayout.attach(this);
-        circleSwipeLayout.addHorizontalScrollableView(findViewById(R.id.horizontalScrollView));
-        circleSwipeLayout.setLayoutCloseListener(new KugouLayout.LayoutCloseListener() {
+        kugouLayout = KugouLayout.attach(this);
+        kugouLayout.addHorizontalScrollableView(findViewById(R.id.horizontalScrollView));
+        kugouLayout.setLayoutCloseListener(new KugouLayout.LayoutCloseListener() {
             @Override
             public void onLayoutClose() {
                 finish();
@@ -39,15 +39,17 @@ public class MainActivity extends ActionBarActivity {
 
         switch (id){
             case R.id.action_normal_anim:
-                circleSwipeLayout.setAnimType(KugouLayout.NORMAL_ANIM);
+                kugouLayout.setAnimType(KugouLayout.NORMAL_ANIM);
                 return true;
             case R.id.action_rebound_anim:
-                circleSwipeLayout.setAnimType(KugouLayout.REBOUND_ANIM);
+                kugouLayout.setAnimType(KugouLayout.REBOUND_ANIM);
                 return true;
             case R.id.action_always_rebound_anim:
-                circleSwipeLayout.setAnimType(KugouLayout.ALWAYS_REBOUND);
+                kugouLayout.setAnimType(KugouLayout.ALWAYS_REBOUND);
                 return true;
         }
         return super.onOptionsItemSelected(item);
     }
+
+
 }
